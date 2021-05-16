1f8c555fe282f1c2a9d21c01ab7c92d34ce01c5c
Tian <tg847@york.ac.uk>
2021-05-16 20:34:42 +0100

    add

--- NARMAv1.0/Narma10.m
+++ NARMAv1.0/Narma10.m
@@ -11,6 +11,7 @@ test_error = zeros(1,loop);
 tic
 for i = 1:loop
 %% Setup
+
 %rng(1,'twister');
 sequenceLength = 2000;
 memoryLength = 10;
@@ -20,10 +21,12 @@ config.memoryLength = '{10,5}'; %[0,0.5]
 [inputSequence, outputSequence] = generate_new_NARMA_sequence(sequenceLength, memoryLength);
 
 %% Time-multiplexing
+
 config.masking_type = 'Random Mask';  % select between 'Binary Mask','Random Mask','Sample and Hold'
 [system_inputSequence] = TimeMultiplexing(inputSequence,nodes,sequenceLength,theta,config);
 
 %% Run Mackey-Glass in Simulink
+
 TFinal = theta * sequenceLength * nodes;
 coupling = 5;
 decay_rate = 1;
@@ -31,25 +34,28 @@ n = 1.2; % Nonlinearity
 config.connect_type = '30'; % Connectivity: '30','15','10','5','2'
 [state_matrix] = Sim_MG(coupling,decay_rate,n,TFinal,config);
 
-%% Training
+%% Training --- ridge regression Wout = BA'(AA'-λI)^-1
+
 [system_train_output_sequence,target_train_output_sequence,system_test_output_sequence,target_test_output_sequence] ...
     = train_test(state_matrix, outputSequence, sequenceLength, nodes);
 
 %% Evaluation
+
 config.err_type = 'NRMSE';
     train_error(i) = calculateError(system_train_output_sequence,target_train_output_sequence,config);
     test_error(i) = calculateError(system_test_output_sequence,target_test_output_sequence,config);
     
-% %% Demultiplexing
-config.plot_type = 'train set';
+%% Demultiplexing
+
+config.plot_type = 'test set';
 [target_plot,system_plot] = demultiplexing(system_train_output_sequence,target_train_output_sequence,...
     system_test_output_sequence,target_test_output_sequence,config);
 
 %% Plot
-plot(target_plot(800:900),'r');
+
+plot(target_plot(800:900),'r-*');
 hold on;
 plot(system_plot(800:900),'b')
-
 
 toc
 end
--- NARMAv1.0/train_test.m
+++ NARMAv1.0/train_test.m
@@ -1,31 +1,34 @@
 function [system_train_output_sequence,target_train_output_sequence,system_test_output_sequence,...
     target_test_output_sequence] = train_test(state_matrix, outputSequence, sequenceLength, nodes)
 
-% Split split of data set 60/20/20 train/test
-train_state = state_matrix(:,1:0.6*nodes*sequenceLength);
-test_state = state_matrix(:,0.6*nodes*sequenceLength +1:0.8*nodes*sequenceLength);
+% Split split of data set 60/20 train/test
+system_train_state = state_matrix(:,1:0.6*nodes*sequenceLength); % A ∈ 
+system_test_state = state_matrix(:,0.6*nodes*sequenceLength +1:0.8*nodes*sequenceLength); % C ∈
 
 % Target output matrix
 target_train_matrix = repmat(outputSequence,1,0.6*nodes); % concatenation of outputSequence: Bl
-target_train_output_sequence = repmat(target_train_matrix,1,sequenceLength);  % concatenation of outputSequence: B
+target_train_output_sequence = repmat(target_train_matrix,1,sequenceLength);  % concatenation of outputSequence: B ∈
 target_test_matrix = repmat(outputSequence,1,0.2*nodes);
-target_test_output_sequence = repmat(target_test_matrix,1,sequenceLength);
+target_test_output_sequence = repmat(target_test_matrix,1,sequenceLength); % D ∈
 
 % Find best reg parameter
 reg_param = [10e-1 10e-3 10e-5 10e-7 10e-9 10e-11];
 
     for i = 1:length(reg_param)
-        %Train: tanspose is inversed compared to equation
-        output_weights = target_train_output_sequence*train_state'* ...
-            inv(train_state*train_state' + reg_param(i)*eye(size(train_state*train_state')));
+        %Training --- Ridge Regression Wout = BA'(AA'-λI)^-1
+
+        output_weights = target_train_output_sequence*system_train_state'* ...
+            inv(system_train_state*system_train_state' + reg_param(i)*eye(size(system_train_state*system_train_state')));
         
         % Trained system output
-        system_train_output_sequence = output_weights*train_state;
+        system_train_output_sequence = output_weights*system_train_state;
        
     end
+    
+    %remove NaNs
     output_weights(isnan(output_weights)) = 0;
 
-    system_test_output_sequence = output_weights * test_state;
+    system_test_output_sequence = output_weights * system_test_state;
     
     
 end

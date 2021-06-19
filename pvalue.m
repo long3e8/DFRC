401b5c0c8004f83fa30808da033da8878a0a8690
Tian <tg847@york.ac.uk>
2021-06-19 08:45:53 +0100

    add

--- pvalue.m
+++ pvalue.m
@@ -1,5 +1,13 @@
 load nonoffset_BM.mat;
-load offset_BM.mat;
-[p,h,stats] = ranksum(nonoffset_BM(:,3),nonoffset_BM(:,2));
+load nonoffset_RM.mat;
 
-d = computeCohen_d(nonoffset_BM(:,2),nonoffset_BM(:,3), 'independent');
+load offset_BM.mat;
+load offset_RM.mat;
+[p,h,stats] = ranksum(BM(:,3),nonoffset_BM(:,3),'alpha',0.05);
+
+% d = computeCohen_d(RM(:,3),BM(:,3), 'paired');
+
+M = size(BM(:,3),1);
+N = size(nonoffset_BM(:,3),1);
+
+A = (stats.ranksum/N - (N+1)/2)/M;

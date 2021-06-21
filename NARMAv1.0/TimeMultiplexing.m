f4c30bf22e96aeffc202b915612f5ee424471bd1
tian <tian@gw.customer.ask4.lan>
2021-06-21 18:57:22 +0100

    add

--- NARMAv1.0/TimeMultiplexing.m
+++ NARMAv1.0/TimeMultiplexing.m
@@ -3,26 +3,29 @@ function [masking] = TimeMultiplexing(inputSequence,sequenceLength,nodes,config)
 switch(config.masking_type)
    
     case '1' % Sample and Hold
-%         rng(1,'twister'); 
 
         masking = repelem (inputSequence,nodes);
         
         
     case '2' % Binary Masking
+        
         AinputSequence = repelem (inputSequence,nodes);
         r = 2* randi(2,nodes,1)-1 -2;
-%         r = randi([0,1],nodes,1);
         Amasking = repmat(r,sequenceLength,1);
+        % Offset
+        masking = Amasking .* AinputSequence + AinputSequence;
+        % No-Offset
+%         masking = Amasking .* AinputSequence; 
 
-%         masking = Amasking .* AinputSequence + AinputSequence;
-        masking = Amasking .* AinputSequence; % M * u
     case '3' % Random Masking
         
         AinputSequence = repelem (inputSequence,nodes);
         r = -1 + (1+1)*rand(nodes,1);
         Amasking = repmat(r,sequenceLength,1);
-%         masking = Amasking .* AinputSequence + AinputSequence;
-        masking = Amasking .* AinputSequence; % M * u
+        % Offset
+        masking = Amasking .* AinputSequence + AinputSequence;
+        % No-Offset
+%         masking = Amasking .* AinputSequence; % M * u
         
         otherwise
         

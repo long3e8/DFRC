238344ca2551c8d8205597df397ca17b4642e85d
Tian <tg847@york.ac.uk>
2021-07-05 00:50:47 +0100

    add

--- NARMAv1.0/connect_plot.m
+++ NARMAv1.0/connect_plot.m
@@ -1,13 +1,14 @@
 load 10_offset.mat;
 load 5_offset.mat;
 load 2_offset.mat;
+load 1_offset.mat
 
 
 % x1 = ['2','5','10','15','30'];
 
 figure('Renderer', 'painters', 'Position', [10 10 800 400]);
 
-t = tiledlayout(1,3);
+t = tiledlayout(1,4);
 
 xlabel(t,'Time-multiplexing');
 ylabel(t,'NRMSE');
@@ -28,7 +29,7 @@ boxplot(test_err_o5,'Color',[0, 0, 0.65],'Notch','on','Labels',{'SH','BWM','RWM'
 % boxplot(train_err_15,'Color',[0, 0.4470, 0.7410],'Notch','on','Labels',{'SH','BM','RM'});
 % xtickangle(ax2,45);
 ylim([0.08 0.65]);
-title('N_i = 5','FontSize', 11);
+title('N_i = 1','FontSize', 11);
 grid on;
 
 ax3 = nexttile;
@@ -40,14 +41,14 @@ ylim([0.08 0.65]);
 title('N_i = 2','FontSize', 11);
 grid on;
 
-% ax4 = nexttile;
-% boxplot(test_err_5,'Color',[0, 0, 0.65],'Notch','on','Labels',{'SH','BWM','RWM'});
-% % hold on;
-% % boxplot(train_err_5,'Color',[0, 0.4470, 0.7410],'Notch','on','Labels',{'SH','BM','RM'});
-% % xtickangle(ax4,45);
-% ylim([0.08 0.65]);
-% title('N_i = 5','FontSize', 11);
-% grid on;
+ax4 = nexttile;
+boxplot(test_err_o1,'Color',[0, 0, 0.65],'Notch','on','Labels',{'SH','BWM','RWM'});
+% hold on;
+% boxplot(train_err_5,'Color',[0, 0.4470, 0.7410],'Notch','on','Labels',{'SH','BM','RM'});
+% xtickangle(ax4,45);
+ylim([0.08 0.65]);
+title('N_i = 5','FontSize', 11);
+grid on;
 
 % ax5 = nexttile;
 % boxplot(test_err_2,'Color',[0, 0, 0.65],'Notch','on','Labels',{'SH','BWM','RWM'});
@@ -74,4 +75,4 @@ lines = findobj(gcf, 'type', 'line', 'Tag', 'Median');
 set(lines, 'Color', [0.8500 0.3250 0.0980]);
 
 % linkaxes([ax1,ax2,ax3,ax4,ax5,ax6],'y');
-linkaxes([ax1,ax2,ax3],'y');
+linkaxes([ax1,ax2,ax3,ax4],'y');

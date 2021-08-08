2057c43f07c267b0c87cd2dd62326c05425e1d30
tian <tian@gw.customer.ask4.lan>
2021-08-08 22:43:41 +0100

    add

--- NARMA/Narma.m
+++ NARMA/Narma.m
@@ -5,32 +5,32 @@
 clear
 close all
 
-loop = 30; % Runs
+loop = 1; % Runs
 train_err = zeros(loop,1);
 test_err = zeros(loop,1); % (loop, 3 for mask / 5 for sampling)
 
-% rng(1,'twister');
+rng(1,'twister');
 
 % sampling = [1,2,5,10]; % 1, 2, 5, 10, 15, 30
-mask = [1,2,3]; % masking --- See TimeMultiplexing.m
+% mask = [1,2,3]; % masking --- See TimeMultiplexing.m
 
 for i = 1:loop
     tic
-    for j = 1:3
+%     for j = 1:3
     %% Setup
     sequenceLength = 3000;
-    memoryLength = 20;
+    memoryLength = 30;
     nodes = 30;
     theta = 0.06;
     tau = nodes * theta;
 
-    config.memoryLength = '20'; %[0,0.5]
+    config.memoryLength = '{10,5}'; %[0,0.5]
 
     [inputSequence, outputSequence] = generate_new_NARMA_sequence(sequenceLength, memoryLength);
 
     %% Time-multiplexing
-%     config.masking_type = '1'; % select between '1 = Sample and Hold','2 = Binary Weight Mask','3 = Random Weight Mask'
-    config.masking_type = num2str(mask(j));
+    config.masking_type = '3'; % select between '1 = Sample and Hold','2 = Binary Weight Mask','3 = Random Weight Mask'
+%     config.masking_type = num2str(mask(j));
     [masking] = TimeMultiplexing(inputSequence,sequenceLength,nodes,config);
     start_time = 0; % Starting time --- in order to make T = TFinal
     N = sequenceLength * nodes; % Number of values
@@ -64,8 +64,8 @@ for i = 1:loop
         train_error = calculateError(system_train_output_sequence,target_train_state,config);
         test_error = calculateError(system_test_output_sequence,target_test_state,config);
 
-        train_err(i,j) = train_error;
-        test_err(i,j) = test_error;
+        train_err(i,1) = train_error;
+        test_err(i,1) = test_error;
 
     %% Demultiplexing
     
@@ -84,7 +84,7 @@ for i = 1:loop
 %     legend('target output','system output')
 
 
-    end
+%     end
     toc
 end
 % save 'N20_offset' test_err_N20offset train_err_N20offset
